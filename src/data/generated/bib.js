087939cc20ca78b0c874126527a32b703d05b5f9
edmbell <edmbell8338@gmail.com>
2025-05-21 13:07:44 +0100

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,117 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+const generatedBibEntries = {
+    "Balakrishnan2018VoxelMorph": {
+        "abstract": "VoxelMorph is an unsupervised CNN-based framework for deformable image registration. Fast and effective for longitudinal MRI analysis.",
+        "author": "Balakrishnan, G. and Zhao, A. and Sabuncu, M. R. and Guttag, J. and Dalca, A. V.",
+        "doi": "10.48550/arXiv.1809.05231",
+        "journal": "arXiv preprint",
+        "keywords": "type:VAE, registration, CNN, longitudinal, brain",
+        "title": "VoxelMorph: A Learning Framework for Deformable Medical Image Registration",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "url": "https://arxiv.org/abs/1809.05231",
+        "year": "2018"
+    },
+    "Blemker2023Springbok": {
+        "abstract": "Springbok\u2019s AI-driven framework converts 2D MRI into detailed 3D muscle models, offering relevant architectural parallels for neuroimaging.",
+        "author": "Blemker, S. and et al.",
+        "doi": "10.1148/ryai.220132",
+        "journal": "Radiology: AI",
+        "keywords": "type:translation, 2D-to-3D, muscle, segmentation, transferable",
+        "title": "Springbok Analytics: Automatic 3D Segmentation from 2D MRI",
+        "type": "article",
+        "year": "2023"
+    },
+    "Dorjsembe2023MedDDPM": {
+        "abstract": "Med-DDPM presents a conditional diffusion model enabling semantic 3D brain MRI synthesis via controlled image generation from annotated guidance.",
+        "author": "Dorjsembe, B. and Pao, C. and Wang, Y.",
+        "doi": "10.48550/arXiv.2305.18453",
+        "journal": "arXiv preprint",
+        "keywords": "type:diffusion, 3D, semantic, conditioning, MRI",
+        "title": "Conditional Diffusion Models for Semantic 3D Brain MRI Synthesis",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2305.18453",
+        "year": "2023"
+    },
+    "Hazra2019SpatioTemporal": {
+        "abstract": "Proposes a statistical framework to model temporal dynamics in MRI using image-on-image regression. Applied to neurodegenerative disease progression.",
+        "author": "Hazra, A. and Reich, B. J. and Reich, D. S. and Shinohara, R. T. and Staicu, A.-M.",
+        "doi": "10.1007/s12561-017-9206-z",
+        "journal": "Statistics in Biosciences",
+        "keywords": "type:statistical, spatio-temporal, regression, longitudinal, brain",
+        "number": "1",
+        "pages": "22--46",
+        "title": "A Spatio-Temporal Model for Longitudinal Image-on-Image Regression",
+        "type": "article",
+        "volume": "11",
+        "year": "2019"
+    },
+    "Kazerouni2022Survey": {
+        "abstract": "A broad survey of diffusion model architectures and applications across medical imaging domains, with attention to 3D extensions.",
+        "author": "Kazerouni, A. and Aghdam, E. K. and Heidari, M. and et al.",
+        "doi": "10.48550/arXiv.2211.07804",
+        "journal": "arXiv preprint",
+        "keywords": "type:survey, diffusion, medical imaging, review",
+        "title": "Diffusion Models in Medical Imaging: A Comprehensive Survey",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2211.07804",
+        "year": "2022"
+    },
+    "Khader2022MedicalDiffusion": {
+        "abstract": "This work adapts DDPMs for volumetric brain MRI, offering improved fidelity and spatial coherence in 3D image synthesis.",
+        "author": "Khader, F. and Mueller-Franzes, G. and Tayebi Arasteh, S. and et al.",
+        "doi": "10.48550/arXiv.2211.03364",
+        "journal": "arXiv preprint",
+        "keywords": "type:diffusion, 3D, MRI, denoising, volumetric",
+        "title": "Medical Diffusion: Denoising Diffusion Probabilistic Models for 3D Medical Image Generation",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2211.03364",
+        "year": "2022"
+    },
+    "Kwon2019AutoEncodingGAN": {
+        "abstract": "Alpha-GAN-based model combining VAE and GAN with code discriminator to generate high-resolution 3D brain MRI from limited data.",
+        "author": "Kwon, G. and Han, C. and Kim, D.",
+        "doi": "10.48550/arXiv.1908.02498",
+        "journal": "arXiv preprint",
+        "keywords": "type:GAN, 3D, brain, synthesis, VAE",
+        "title": "Generation of 3D Brain MRI Using Auto-Encoding Generative Adversarial Networks",
+        "type": "article",
+        "url": "https://arxiv.org/abs/1908.02498",
+        "year": "2019"
+    },
+    "Litrico2024TADM": {
+        "abstract": "TADM introduces a temporally-aware diffusion model guided by a pre-trained Brain-Age Estimator to predict longitudinal brain MRI scans. It demonstrates strong performance on OASIS-3 data.",
+        "author": "Litrico, M. and Guarnera, F. and Giuffrida, M.V. and Rav\u00ec, D. and Battiato, S.",
+        "booktitle": "Medical Image Computing and Computer-Assisted Intervention \u2013 MICCAI 2024",
+        "doi": "10.1007/978-3-031-72069-7_42",
+        "keywords": "type:brain, MRI, 2D, prediction, longitudinal, diffusion",
+        "publisher": "Springer",
+        "title": "TADM: Temporally-Aware Diffusion Model for Neurodegenerative Progression on Brain MRI",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "MuellerFranzes2024DDPMvsGAN": {
+        "abstract": "Compares DDPMs and GANs for low-dose breast MRI reconstruction, offering insights into trade-offs and generalizability of generative models.",
+        "author": "M\u00fcller-Franzes, G. and Niehues, J. M. and Khader, F. and et al.",
+        "doi": "10.1186/s41747-024-00451-3",
+        "journal": "European Radiology Experimental",
+        "keywords": "type:comparison, GAN, DDPM, low-dose, breast MRI",
+        "number": "1",
+        "pages": "1--10",
+        "title": "Diffusion probabilistic versus generative adversarial models to reduce contrast agent dose in breast MRI",
+        "type": "article",
+        "volume": "8",
+        "year": "2024"
+    },
+    "Zhao2021AlzheimersGAN": {
+        "abstract": "Presents a GAN-based method to predict future brain MRIs showing Alzheimer's progression, leveraging baseline scans from the ADNI dataset.",
+        "author": "Zhao, Y. and Ma, B. and Jiang, P. and Zeng, D. and Wang, X. and Li, S.",
+        "doi": "10.1109/JBHI.2020.3006925",
+        "journal": "IEEE Journal of Biomedical and Health Informatics",
+        "keywords": "type:GAN, Alzheimer, forecasting, longitudinal, brain",
+        "number": "3",
+        "pages": "711--719",
+        "title": "Prediction of Alzheimer's Disease Progression with Multi-Information Generative Adversarial Network",
+        "type": "article",
+        "volume": "25",
+        "year": "2021"
     }
 };
